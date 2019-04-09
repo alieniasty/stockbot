82581614b79a9d25ca04e317215f3327df72cfd8
Adrian Jagodzinski <adrian.jagodzinski@savelend.se>
2019-04-09 15:43:17 +0200

    Initial unit tests, basic parser and solution structure

---
+++ Parser.Tests/Tests.fs
@@ -0,0 +1,29 @@
+﻿module Parser.Tests
+
+open Xunit
+open Parser
+open System
+
+type ``Parser tests``() =
+    
+    [<Theory>]
+    [<InlineData("@AnotherBot LNKD 1/1/2000 31/12/2015", "@AnotherBot", "LNKD")>]
+    [<InlineData("@StockBot MSFT 1/1/2000 31/12/2015", "@StockBot", "MSFT")>]
+    member this.``Given correct tweet, Parser constructs proper record.``(text, sender, ticker) =
+        let expected =
+            {
+                Sender = sender
+                Ticker = ticker
+                From = DateTime(2000, 1, 1)
+                Until = DateTime(2015, 12, 31)
+            } |> Outcome.Success
+        let actual = Parse text
+        Assert.Equal(expected, actual)
+        
+    [<Theory>]
+    [<InlineData("@Ano", "@AnotherBot", "LNKD")>]
+    [<InlineData("/1/2000 31/12/2015", "@StockBot", "MSFT")>]
+    member this.``Given incorrect tweet, Parser constructs proper record.``(text, sender, ticker) =
+        let expected = Outcome.Failure "Invalid text"
+        let actual = Parse text
+        Assert.Equal(expected, actual)
---
+++ Parser/Parser.fs
@@ -0,0 +1,34 @@
+﻿module Parser
+
+open System
+open System.Globalization
+
+    type Outcome<'S> =
+    | Success of result:'S
+    | Failure of message:string
+
+    type Query =
+        {
+            Sender : string
+            Ticker : string
+            From : DateTime
+            Until : DateTime
+        }
+    
+    let private parseDate (s : string) =
+        DateTime.ParseExact(s,
+                            "d/M/yyyy",
+                            CultureInfo.InvariantCulture,
+                            DateTimeStyles.AssumeLocal)
+      
+    let Parse (text : string) =
+        let elements = text.Split ' '
+        match elements with
+        | [|sender; ticker; from; until|] ->
+            {
+                Sender = elements.[0]
+                Ticker = elements.[1]
+                From = elements.[2] |> parseDate
+                Until = elements.[3] |> parseDate
+            } |> Outcome.Success
+        | _ -> Outcome.Failure "Invalid text"
